ee80766be6cfe54c2217562012140b086d39e80a
sium01 <156007391+sium01@users.noreply.github.com>
2025-02-15 04:01:25 +0600

    Update productmodel.js

--- models/productmodel.js
+++ models/productmodel.js
@@ -1,10 +1,16 @@
 const mongoose=require('mongoose');
+const { type } = require('os');
 const product=mongoose.Schema(
  
  {
 name:{
  type:String,
  require:[true,"Please enter the product name"]
+},price:{
+ type:Number,
+ required:true,
+ default:0
+
 }
 
 
